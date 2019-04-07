835ac5bf97d414095c94a19e1504af03fe003e97
Bhanuka25 <bhanukamax@gmail.com>
2019-04-07 21:36:21 +0530

    cached everything

--- sw.js
+++ sw.js
@@ -1,26 +1,20 @@
 console.log('tesssss');
 var urlsToCache = ['/', '/assets/styles.css'];
+cacheName = 'v1';
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  console.log('installlllled ');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    }),
-  );
 });
-self.addEventListener('fetch', function(event) {
-  console.log('fetccccching');
-  console.log({event});
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    }),
+self.addEventListener('fetch', e => {
+    console.log('request ', e.request);
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(cacheName).then(cache => {
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(error => caches.match(e.request).then(res => res)),
   );
 });
